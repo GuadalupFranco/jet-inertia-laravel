3ee801af03ae060520911025da55dffa79535f17
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-27 18:22:37 -0500

    add list table and project dependencies

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,58 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+defineProps({
+    notes: Array,
+});
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notes module
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="p3-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">List</h3>
+                            <p class="text-sm text-gray-600">
+                                Select any option (see, edit, remove)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="(note, index) in notes" :key="index">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.show', note)">
+                                            See
+                                        </inertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.edit', note)">
+                                            Edit
+                                        </inertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.destroy', notes)">
+                                            Remove
+                                        </inertia-link>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
